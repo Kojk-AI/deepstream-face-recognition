b17a6f4eb6002e8e964376173b444d36127f7d3a
Kojk-AI <107860554+Kojk-AI@users.noreply.github.com>
2022-06-23 23:43:35 +0800

    Add files via upload

---
+++ app/deepstream-face-rtsp.py
@@ -0,0 +1,387 @@
+#!/usr/bin/env python3
+
+################################################################################
+# SPDX-FileCopyrightText: Copyright (c) 2020-2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+import argparse
+import sys
+sys.path.append('../')
+import configparser
+from ctypes import *
+import ctypes
+import numpy as np
+
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version('GstRtspServer', '1.0')
+from gi.repository import GObject, Gst, GstRtspServer
+from common.is_aarch_64 import is_aarch64
+from common.bus_call import bus_call
+
+import pyds
+
+past_tracking_meta=[0]
+
+emb_array =[]
+names =[]
+id_name_dic = {}
+
+def osd_sink_pad_buffer_probe(pad,info,u_data):
+    face_thres = 1.0 
+    
+    with np.load('/app/models/embeddings.npz') as embeddings:
+        for key in embeddings.keys():
+            emb_array.append(embeddings[key])
+
+
+    with open("/app/models/names.txt") as f:
+        lines = f.readlines()
+        for line in lines:
+            names.append(line.split()[0])        
+        
+    gst_buffer = info.get_buffer()
+    if not gst_buffer:
+        print("Unable to get GstBuffer ")
+        return
+
+    # Retrieve batch metadata from the gst_buffer
+    # Note that pyds.gst_buffer_get_nvds_batch_meta() expects the
+    # C address of gst_buffer as input, which is obtained with hash(gst_buffer)
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            # Note that l_frame.data needs a cast to pyds.NvDsFrameMeta
+            # The casting is done by pyds.NvDsFrameMeta.cast()
+            # The casting also keeps ownership of the underlying memory
+            # in the C code, so the Python garbage collector will leave
+            # it alone.
+           frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        l_obj=frame_meta.obj_meta_list
+        while l_obj is not None:
+            try:
+                # Casting l_obj.data to pyds.NvDsObjectMeta
+                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+
+            # If Tracker is enabled only
+            # Retrieve face recognition id for tracked object id
+            # Replaces display name for bbox with face recognition id        
+            if str(obj_meta.object_id) in id_name_dic.keys():
+                obj_meta.text_params.display_text = id_name_dic[str(obj_meta.object_id)]      
+
+            
+            l_user = obj_meta.obj_user_meta_list
+
+            # Retrieve TensorMeta data from FaceNet
+            # Face recognition is this example is carried out with a simple L2 distance between the embedding from FaceNet
+            # with a list of pre-saved embeddings corresponding to the face ids (one in this case)
+            if l_user is not None:
+                user_meta = pyds.NvDsUserMeta.cast(l_user.data)
+                tensor_meta = pyds.NvDsInferTensorMeta.cast(user_meta.user_meta_data)
+                layer = pyds.get_nvds_LayerInfo(tensor_meta, 0)
+                ptr = ctypes.cast(pyds.get_ptr(layer.buffer), ctypes.POINTER(ctypes.c_float))
+                v = np.ctypeslib.as_array(ptr, shape=(1,128))
+                
+                # L2 normalization
+                v = v/np.linalg.norm(v)
+                
+                for i, emb in enumerate(emb_array):
+                    dist = np.linalg.norm(v - emb)
+                    if dist < face_thres:
+                        id_name_dic[str(obj_meta.object_id)]= names[i]
+                        break
+            
+            try: 
+                l_obj=l_obj.next
+            except StopIteration:
+                break
+
+        try:
+            l_frame=l_frame.next
+        except StopIteration:
+            break
+			
+    return Gst.PadProbeReturn.OK	
+
+
+def main(args):
+    # Standard GStreamer initialization
+    GObject.threads_init()
+    Gst.init(None)
+
+    # Create gstreamer elements
+    # Create Pipeline element that will form a connection of other elements
+    print("Creating Pipeline \n ")
+    pipeline = Gst.Pipeline()
+
+    if not pipeline:
+        sys.stderr.write(" Unable to create Pipeline \n")
+
+    # Source element for reading from the file
+    print("Creating Source \n ")
+    source = Gst.ElementFactory.make("nvarguscamerasrc", "csi-cam-source")
+    if not source:
+        sys.stderr.write(" Unable to create Source \n")
+
+    print("Creating Video Converter \n")
+
+    # nvvideoconvert to convert incoming raw buffers to NVMM Mem (NvBufSurface API)
+    nvvidconvsrc = Gst.ElementFactory.make("nvvideoconvert", "convertor_src")
+    if not nvvidconvsrc:
+        sys.stderr.write(" Unable to create Nvvideoconvert \n")
+
+    caps_vidconvsrc = Gst.ElementFactory.make("capsfilter", "nvmm_caps")
+    if not caps_vidconvsrc:
+        sys.stderr.write(" Unable to create capsfilter \n")
+
+    # Create nvstreammux instance to form batches from one or more sources.
+    streammux = Gst.ElementFactory.make("nvstreammux", "Stream-muxer")
+    if not streammux:
+        sys.stderr.write(" Unable to create NvStreamMux \n")
+
+    # Use nvinfer to run inferencing on camera's output,
+    # behaviour of inferencing is set through config file
+    pgie = Gst.ElementFactory.make("nvinfer", "primary-inference")
+    if not pgie:
+        sys.stderr.write(" Unable to create pgie \n")
+
+    tracker = Gst.ElementFactory.make("nvtracker", "tracker")
+    if not tracker:
+        sys.stderr.write(" Unable to create tracker \n")
+
+    sgie1 = Gst.ElementFactory.make("nvinfer", "secondary1-nvinference-engine")
+    if not sgie1:
+        sys.stderr.write(" Unable to make sgie1 \n")
+        
+    # Use convertor to convert from NV12 to RGBA as required by nvosd
+    nvvidconv = Gst.ElementFactory.make("nvvideoconvert", "convertor")
+    if not nvvidconv:
+        sys.stderr.write(" Unable to create nvvidconv \n")
+
+    # Create OSD to draw on the converted RGBA buffer
+    nvosd = Gst.ElementFactory.make("nvdsosd", "onscreendisplay")
+
+    if not nvosd:
+        sys.stderr.write(" Unable to create nvosd \n")
+
+    # # Finally render the osd output
+    # if is_aarch64():
+    #     transform = Gst.ElementFactory.make("nvegltransform", "nvegl-transform")
+
+    # print("Creating EGLSink \n")
+    # sink = Gst.ElementFactory.make("nveglglessink", "nvvideo-renderer")
+    # if not sink:
+    #     sys.stderr.write(" Unable to create egl sink \n")
+    # sink.set_property('sync', 0)
+
+# RTSP replacement
+    nvvidconv_postosd = Gst.ElementFactory.make("nvvideoconvert", "convertor_postosd")
+    if not nvvidconv_postosd:
+        sys.stderr.write(" Unable to create nvvidconv_postosd \n")
+    
+    # Create a caps filter
+    caps = Gst.ElementFactory.make("capsfilter", "filter")
+    caps.set_property("caps", Gst.Caps.from_string("video/x-raw(memory:NVMM), format=I420"))
+    
+    # Make the encoder
+    if codec == "H264":
+        encoder = Gst.ElementFactory.make("nvv4l2h264enc", "encoder")
+        print("Creating H264 Encoder")
+    elif codec == "H265":
+        encoder = Gst.ElementFactory.make("nvv4l2h265enc", "encoder")
+        print("Creating H265 Encoder")
+    if not encoder:
+        sys.stderr.write(" Unable to create encoder")
+    encoder.set_property('bitrate', bitrate)
+    if is_aarch64():
+        encoder.set_property('preset-level', 1)
+        encoder.set_property('insert-sps-pps', 1)
+        encoder.set_property('bufapi-version', 1)
+    
+    # Make the payload-encode video into RTP packets
+    if codec == "H264":
+        rtppay = Gst.ElementFactory.make("rtph264pay", "rtppay")
+        print("Creating H264 rtppay")
+    elif codec == "H265":
+        rtppay = Gst.ElementFactory.make("rtph265pay", "rtppay")
+        print("Creating H265 rtppay")
+    if not rtppay:
+        sys.stderr.write(" Unable to create rtppay")
+    
+    # Make the UDP sink
+    updsink_port_num = 5400
+    sink = Gst.ElementFactory.make("udpsink", "udpsink")
+    if not sink:
+        sys.stderr.write(" Unable to create udpsink")
+    
+    sink.set_property('host', '224.224.255.255')
+    sink.set_property('port', updsink_port_num)
+    sink.set_property('async', False)
+    sink.set_property('sync', 0)
+
+####################
+
+    print("Playing cam %s " %stream_path)
+    caps_vidconvsrc.set_property('caps', Gst.Caps.from_string("video/x-raw(memory:NVMM), width=1280, height=720, framerate=60/1"))
+    source.set_property('bufapi-version', True)
+    streammux.set_property('width', 1280)
+    streammux.set_property('height', 720)
+    streammux.set_property('batch-size', 1)
+    streammux.set_property('live-source', 1)
+    streammux.set_property('batched-push-timeout', 4000000)
+    pgie.set_property('config-file-path', "/app/configs/deepstream_pgie_config.txt")
+    sgie1.set_property('config-file-path', "/app/configs/deepstream_sgie1_config.txt")
+
+    #Set properties of tracker
+    config = configparser.ConfigParser()
+    config.read('/app/configs/deepstream_tracker_config.txt')
+    config.sections()
+
+    for key in config['tracker']:
+        if key == 'tracker-width' :
+            tracker_width = config.getint('tracker', key)
+            tracker.set_property('tracker-width', tracker_width)
+        if key == 'tracker-height' :
+            tracker_height = config.getint('tracker', key)
+            tracker.set_property('tracker-height', tracker_height)
+        if key == 'gpu-id' :
+            tracker_gpu_id = config.getint('tracker', key)
+            tracker.set_property('gpu_id', tracker_gpu_id)
+        if key == 'll-lib-file' :
+            tracker_ll_lib_file = config.get('tracker', key)
+            tracker.set_property('ll-lib-file', tracker_ll_lib_file)
+        if key == 'll-config-file' :
+            tracker_ll_config_file = config.get('tracker', key)
+            tracker.set_property('ll-config-file', tracker_ll_config_file)
+        if key == 'enable-batch-process' :
+            tracker_enable_batch_process = config.getint('tracker', key)
+            tracker.set_property('enable_batch_process', tracker_enable_batch_process)
+        if key == 'enable-past-frame' :
+            tracker_enable_past_frame = config.getint('tracker', key)
+            tracker.set_property('enable_past_frame', tracker_enable_past_frame)
+        if key == 'display-tracking-id' :
+            tracker_display_tracking_id = config.getint('tracker', key)
+            tracker.set_property('display-tracking-id', tracker_display_tracking_id)
+            
+    print("Adding elements to Pipeline \n")
+    pipeline.add(source)
+    pipeline.add(nvvidconvsrc)
+    pipeline.add(caps_vidconvsrc)
+    pipeline.add(streammux)
+    pipeline.add(pgie)
+    pipeline.add(tracker)
+    pipeline.add(sgie1)
+    pipeline.add(nvvidconv)
+    pipeline.add(nvosd)
+    pipeline.add(nvvidconv_postosd)
+    pipeline.add(caps)
+    pipeline.add(encoder)
+    pipeline.add(rtppay)
+    pipeline.add(sink)
+
+
+
+    print("Linking elements in the Pipeline \n")
+    source.link(nvvidconvsrc)
+    nvvidconvsrc.link(caps_vidconvsrc)
+
+    sinkpad = streammux.get_request_pad("sink_0")
+    if not sinkpad:
+        sys.stderr.write(" Unable to get the sink pad of streammux \n")
+    srcpad = caps_vidconvsrc.get_static_pad("src")
+    if not srcpad:
+        sys.stderr.write(" Unable to get source pad of caps_vidconvsrc \n")
+    srcpad.link(sinkpad)
+
+    streammux.link(pgie)
+    pgie.link(tracker)
+    tracker.link(sgie1)
+    sgie1.link(nvvidconv)
+    nvvidconv.link(nvosd)
+    nvosd.link(nvvidconv_postosd)
+    nvvidconv_postosd.link(caps)
+    caps.link(encoder)
+    encoder.link(rtppay)
+    rtppay.link(sink)
+
+    # create an event loop and feed gstreamer bus mesages to it
+    loop = GObject.MainLoop()
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect ("message", bus_call, loop)
+    
+    # Add RTSP Streaming
+    # Start streaming
+    rtsp_port_num = 8554
+    
+    server = GstRtspServer.RTSPServer.new()
+    server.props.service = "%d" % rtsp_port_num
+    server.attach(None)
+    
+    factory = GstRtspServer.RTSPMediaFactory.new()
+    factory.set_launch( "( udpsrc name=pay0 port=%d caps=\"application/x-rtp, media=video, clock-rate=90000, encoding-name=(string)%s, payload=96 \" )" % (updsink_port_num, codec))
+    factory.set_shared(True)
+    server.get_mount_points().add_factory("/ds-test", factory)
+
+######################
+    
+    # Lets add probe to get informed of the meta data generated, we add probe to
+    # the sink pad of the osd element, since by that time, the buffer would have
+    # had got all the metadata.
+    osdsinkpad = nvosd.get_static_pad("sink")
+    if not osdsinkpad:
+        sys.stderr.write(" Unable to get sink pad of nvosd \n")
+
+    osdsinkpad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, 0)
+
+    # start play back and listen to events
+    print("Starting pipeline \n")
+    pipeline.set_state(Gst.State.PLAYING)
+    try:
+        loop.run()
+    except:
+        pass
+    # cleanup
+    pipeline.set_state(Gst.State.NULL)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Application Help')
+    parser.add_argument("-i", "--input", default="/dev/video0",
+                  help="Path to v4l2 device path such as /dev/video0")
+
+    args = parser.parse_args()
+
+    global codec
+    global bitrate
+    global stream_path
+    global past_tracking
+
+    codec = "H264"
+    bitrate = 8000000
+    stream_path = args.input
+    past_tracking = 0
+    return 0
+    
+if __name__ == '__main__':
+    parse_args()
+    sys.exit(main(sys.argv))
+
